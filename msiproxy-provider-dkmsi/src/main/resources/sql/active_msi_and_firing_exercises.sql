0aad7c9a000c358841e74f1ba990001417b8a08b
peder280370 <peder@carolus.dk>
2015-01-16 11:41:12 +0100

    MSINM-165: Make MSI-proxy work under apache

--- msiproxy-provider-dkmsi/src/main/resources/sql/active_msi_and_firing_exercises.sql
+++ msiproxy-provider-dkmsi/src/main/resources/sql/active_msi_and_firing_exercises.sql
@@ -9,15 +9,18 @@
     m.sortOrder         AS msg_sort_order
   FROM
     message m
-    LEFT JOIN user u ON m.usernameId = u.id
-    LEFT JOIN organisation o ON o.id = u.organisationId
-    LEFT JOIN country uc ON uc.id = o.countryId
+
+--  The MSI admin use these criteria (where uc.abbreviation = 'DK') ... but we do not want access to the user table
+--    LEFT JOIN user u ON m.usernameId = u.id
+--    LEFT JOIN organisation o ON o.id = u.organisationId
+--    LEFT JOIN country uc ON uc.id = o.countryId
+
     LEFT JOIN location loc ON m.locationId = loc.id
     LEFT JOIN locationtype loctp ON loc.locationTypeId = loctp.id
     LEFT JOIN main_area a ON loc.areaId = a.id
     LEFT JOIN country c ON a.countryId = c.id
   WHERE
-    uc.abbreviation = 'DK'
+    c.abbreviation = 'DK'
     AND m.validFrom < now() + INTERVAL 1 DAY
     AND (m.validTo IS NULL OR m.validTo > now())
     AND m.isLatest = 1
---
+++ msiproxy-web/src/main/java/dk/dma/msiproxy/web/SendRedirectFilter.java
@@ -0,0 +1,106 @@
+package dk.dma.msiproxy.web;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.IOException;
+
+/**
+ * When running HTTPS behind, say, an Apache web server which handles the SSL decoding,
+ * Response.sendRedirect() will not work properly when redirecting to a relative path.
+ * Since the Servlet container receives a HTTP request, the redirect URL will be
+ * using HTTP as well.
+ * <p>
+ *     If, however, the port-443 VirtualHost is configured to set the header originalScheme=https,
+ *     then this filter will ensure that https is used in relative redirects.
+ * </p>
+ * <p>Example configuration:</p>
+ * <pre>
+ *     Header add originalScheme "https"
+ *     RequestHeader set originalScheme "https"
+ * </pre>
+ */
+@WebFilter(urlPatterns={"/*"})
+public class SendRedirectFilter  implements Filter {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void destroy() {
+    }
+
+
+    /**
+     * Main filter method
+     * @param req the request
+     * @param res the response
+     * @param chain the filter chain
+     */
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        chain.doFilter(request, new SendRedirectResponse(request, response));
+    }
+
+}
+
+/**
+ * Overrides sendRedict() to make relative locations absolute and adopt the original scheme
+ */
+class SendRedirectResponse extends HttpServletResponseWrapper {
+
+    private static final String HEADER_ORIGINAL_SCHEME = "originalScheme";
+
+    private HttpServletRequest request;
+
+    /**
+     * Constructor
+     * @param request the request
+     * @param response the response
+     */
+    public SendRedirectResponse(HttpServletRequest request, HttpServletResponse response) {
+        super(response);
+        this.request = request;
+    }
+
+    /**
+     * Overrides sendRedict() to make relative locations absolute and adopt the original scheme
+     * @param location the location to redirect to
+     */
+    @Override
+    public void sendRedirect(String location) throws IOException {
+
+        // Only tamper with relative locations
+        int offset = request.getRequestURL().indexOf(request.getRequestURI());
+        boolean wasHttps = "https".equalsIgnoreCase(request.getHeader(HEADER_ORIGINAL_SCHEME));
+        if (!location.toLowerCase().startsWith("http") &&
+                wasHttps &&
+                offset != -1) {
+            // Convert to absolute URL
+            location = request.getRequestURL().substring(0, offset) + location;
+            // Change the scheme to https
+            if (location.toLowerCase().startsWith("http://")) {
+                location = "https://" + location.substring("http://".length());
+            }
+        }
+
+        super.sendRedirect(location);
+    }
+}
