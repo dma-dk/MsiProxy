f96494995206134ed8e85cc80ba93ec353981828
peder280370 <peder@carolus.dk>
2015-01-22 14:45:04 +0100

    Add copyright notices

--- msiproxy-common/src/main/java/dk/dma/msiproxy/common/provider/Providers.java
+++ msiproxy-common/src/main/java/dk/dma/msiproxy/common/provider/Providers.java
@@ -1,3 +1,18 @@
+/* Copyright (c) 2011 Danish Maritime Authority
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package dk.dma.msiproxy.common.provider;
 
 import dk.dma.msiproxy.common.util.CdiHelper;
@@ -13,7 +28,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.naming.NamingException;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
