c8610586a0c5680961999db7287f46092a99cd21
HenrikWintherJ <hje@dma.dk>
2015-10-26 12:40:54 +0100

    Mysql did not expose 3306 port. swithcing the startup line to after nginx fixed this. No clue why!

--- docker/startup.sh
+++ docker/startup.sh
@@ -8,8 +8,8 @@ docker kill $(docker ps | awk '{print $1}')
 docker rm $(docker ps -a | awk '{print $1}')
 docker run -d --name oldmsi dmadk/msi_msiproxy_mysql:v1-0
 sleep 10
-docker run -d -it --link oldmsi:oldmsi dmadk/msi_msiproxy_legacy-import:v1-0
 docker run -p 80:80 -p 443:443  --net=host -v /etc/certs:/etc/certs:ro -d dmadk/msi_msiproxy_nginx:v1-0
+docker run -d -it --link oldmsi:oldmsi dmadk/msi_msiproxy_legacy-import:v1-0
 sleep 15
 docker run -d -e publishTwitterApiKey="CqgrxkIiBA3sC35TmoZ5F5Oru" -e publishTwitterApiSecret="xZXl9vsW3LCtX1Py6U2VqYUmyAK0GGYZ4RINFyXgNwV7PPcQip"\
      -e publishTwitterAccessToken="2829892014-kqkkQLD88xhfakDlbxY0rUPdRA72Nw14e6KED0n"\
